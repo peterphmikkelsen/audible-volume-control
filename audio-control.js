d13fadd6ca3e36b56069a3ab99627c1012306386
Peter Mikkelsen <peterphmikkelsen@protonmail.com>
2026-04-01 17:57:35 +0200

    🐛 fix reset of slider bug in firefox
    this also includes a general cleanup of the code

--- audio-control.js
+++ audio-control.js
@@ -1,35 +1,46 @@
-console.log(
-  "Audible Volume Control - Active! Slide the volume controller and hear the magic happen!",
-);
+window.onload = () => {
+  console.log(
+    "Audible Volume Control - Active! Slide the volume controller and hear the magic happen!"
+  );
+  injectVolumeSlider();
+};
 
-var progress = window.localStorage.getItem("progress");
+async function injectVolumeSlider() {
+  const progress = window.localStorage.getItem("progress") || "100";
+  const bookmarkMenu = document.querySelector("[class*='_playerMenu']");
 
-if (progress === null) {
-  progress = "100";
+  // Check if we already injected it to avoid duplicates
+  if (document.getElementById("audio-control-outer-div")) return;
+
+  bookmarkMenu.insertAdjacentHTML("afterend", `<div class="bc-row" id="audio-control-outer-div"><div id="volume-icon"><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path fill="#343434" d="m4 6v8l5.2 3.9c.3.3.8 0 .8-.5v-14.8c0-.5-.5-.8-.8-.5zm0 8h-3a1 1 0 0 1 -1-1v-6a1 1 0 0 1 1-1h3m0 0"/><path fill="#343434" d="m16.4 17.4a1 1 0 0 1 -.7-1.7 8 8 0 0 0 0-11.4 1 1 0 0 1 1.3-1.3 10 10 0 0 1 0 14.2 1 1 0 0 1 -.7.3z"/><path fill="#343434" d="m13.5 14.5a1 1 0 0 1 -.7-.3 1 1 0 0 1 0-1.4 4 4 0 0 0 0-5.6 1 1 0 0 1 1.4-1.4 6 6 0 0 1 0 8.4 1 1 0 0 1 -.7.3z"/></svg></div><div id="slider-and-progress" class="bc-range"><div id="audio-progress"></div><input id="audio-slider" type="range" min="0" max="100" value="${progress}"></div></div>`)
+
+  // Weird hack for Chromium based browsers - the alignment is not the same
+  if (!!window.chrome) {
+    document.getElementById("slider-and-progress").style.paddingTop = "0.55rem";
+  }
+
+  const audioSlider = document.getElementById("audio-slider");
+  const audioProgress = document.getElementById("audio-progress");
+  const audio = document.getElementById("audible-player");
+
+  audioProgress.style.width = `${progress}%`;
+  audio.volume = perceptualToAmplitude(progress);
+
+  audioSlider.oninput = (event) => {
+    const currProgress = event.target.value;
+    audioProgress.style.width = `${currProgress}%`;
+    audio.volume = perceptualToAmplitude(currProgress);
+    window.localStorage.setItem("progress", currProgress);
+  };
+
+  ["mousedown", "mousemove", "mouseup", "mouseenter", "mouseleave", "focus", "blur"].forEach(evt => {
+    audioSlider.addEventListener(evt, stopPropagation, { capture: true, passive: false });
+  });
 }
 
-var bookmarkMenu = document.querySelector('[class*="_playerMenu"]');
-
-bookmarkMenu.insertAdjacentHTML("afterend", `<div class="bc-row" id="audio-control-outer-div"><div id="volume-icon"><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path fill="#343434" d="m4 6v8l5.2 3.9c.3.3.8 0 .8-.5v-14.8c0-.5-.5-.8-.8-.5zm0 8h-3a1 1 0 0 1 -1-1v-6a1 1 0 0 1 1-1h3m0 0"/><path fill="#343434" d="m16.4 17.4a1 1 0 0 1 -.7-1.7 8 8 0 0 0 0-11.4 1 1 0 0 1 1.3-1.3 10 10 0 0 1 0 14.2 1 1 0 0 1 -.7.3z"/><path fill="#343434" d="m13.5 14.5a1 1 0 0 1 -.7-.3 1 1 0 0 1 0-1.4 4 4 0 0 0 0-5.6 1 1 0 0 1 1.4-1.4 6 6 0 0 1 0 8.4 1 1 0 0 1 -.7.3z"/></svg></div><div id="slider-and-progress" class="bc-range"><div id="audio-progress"></div><input id="audio-slider" type="range" min="0" max="100" value="${progress}"></div></div>`)
-
-var audioSlider = document.getElementById("audio-slider");
-var audioProgress = document.getElementById("audio-progress");
-
-// Weird hack for Chromium based browsers - the alignment is not the same
-if (!!window.chrome) {
-  document.getElementById("slider-and-progress").style.paddingTop = "0.55rem";
-}
-
-audioProgress.style.width = `${progress}%`;
-document.getElementsByTagName("audio")[0].volume =
-  perceptualToAmplitude(progress);
-
-audioSlider.oninput = (event) => {
-  const currProgress = event.target.value;
-  audioProgress.style.width = `${currProgress}%`;
-  document.getElementsByTagName("audio")[0].volume =
-    perceptualToAmplitude(currProgress);
-  window.localStorage.setItem("progress", currProgress);
+function stopPropagation(event) {
+  event.stopPropagation();
+  event.stopImmediatePropagation();
 };
 
 // https://github.com/discord/perceptual/blob/master/src/index.ts
